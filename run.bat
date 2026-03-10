b5c9b48ef85249d9d33000b7280cff15d3503e22
Kumashiro <yowwapabam@gmail.com>
2026-03-11 00:29:19 +0700

    fixed: run.bat & Add: db

--- run.bat
+++ run.bat
@@ -2,6 +2,6 @@
 echo Starting Learnaja
 
 start "Frontend - NPM Install & Vite Dev Server" cmd /k "cd /d "%~dp0frontend" && npm install && npm run dev"
-start "Backend - NPM Install & Nodemon" cmd /k "cd /d "%~dp0backend" && npm install && nodemon index.js"
+start "Backend - NPM Install & Nodemon" cmd /k "cd /d "%~dp0backend" && npm install && npm run dev"
 
 echo Both servers launched
